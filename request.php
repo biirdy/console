8403ecd29026d9097fd4ce4f9a6e4ef64d76f69b
Ubuntu <ubuntu@ip-172-31-32-126.eu-west-1.compute.internal>
2015-02-14 21:27:28 +0000

    update

---
+++ request.php
@@ -0,0 +1,26 @@
+<?php
+	if(isset($_POST['request_type'])){
+		if(strcmp($_POST['request_type'], 'ping') == 0){
+			//should check if all posts are set
+			exec('sudo /home/ubuntu/server/rpc_request ping '. $_POST['sensor_id'] . ' ' . $_POST['rtt_ittr'], $output, $return);
+		}else if(strcmp($_POST['request_type'], 'iperf') == 0){
+			//should check if all posts are set
+			exec('sudo /home/ubuntu/server/rpc_request iperf '. $_POST['sensor_id'] . ' ' . $_POST['bw_time'], $output, $return);
+		}else if(strcmp($_POST['request_type'], 'udp') == 0){
+			//should check if all posts are set
+			exec('sudo /home/ubuntu/server/rpc_request udp '. $_POST['sensor_id'] . ' ' . $_POST['udp_speed'] . ' ' . $_POST['udp_size'] . ' ' . $_POST['udp_time'], $output, $return);
+		}else if(strcmp($_POST['request_type'], 'dns') == 0){
+			//should check if all posts are set
+			///
+		}else{
+			echo("Unknown request type");
+			exit;
+		}
+		foreach ($output as $value) {
+       		echo($value."\n");
+        }
+	}else{
+		echo("Missing request type");
+	}
+	exit;
+?>
--- test.php
+++ test.php
@@ -11,6 +11,27 @@ if (isset($_POST['action'])) {
             exec("mkdir stop");
             echo("stop2");
             break;
+        case 'ping':
+            echo(system("/home/ubuntu/tools/ping ". $_POST['sensor_id'] ." 5 &"));
+            break;
+        case 'server_state':
+            exec("service tnp-server status", $output, $return);
+            echo($return);
+            break;
+        case 'server_start':
+            exec("/usr/bin/sudo /usr/bin/service tnp-server start > /dev/null &", $output, $return);
+            break;
+        case 'server_restart':
+            exec("sudo service tnp-server stop", $output, $return);
+            exec("/usr/bin/sudo /usr/bin/service tnp-server start > /dev/null &", $output, $return);
+            break;
+        case 'server_stop':
+            exec("sudo service tnp-server stop", $output, $return);
+            foreach ($output as $value) {
+                echo("OUTPUT".$value);
+            }
+            echo("RETURN".$return);
+            break;
     }
 }
 
