4c2581160159bbc7577750581fb210ce66d7db75
Ubuntu <ubuntu@ip-172-31-32-126.eu-west-1.compute.internal>
2015-03-03 03:29:13 +0000

    remove absolute paths from server interface to be more compatible

--- server.php
+++ server.php
@@ -2,18 +2,18 @@
 if (isset($_POST['action'])) {
     switch ($_POST['action']) {
         case 'server_state':
-            exec("service network-sensor-server status", $output, $return);
+            exec("sudo service network-sensor-server status", $output, $return);
             echo($return);
             break;
         case 'server_start':
-            exec("/usr/bin/sudo /usr/bin/service network-sensor-server start > /dev/null &", $output, $return);
+            exec("sudo service network-sensor-server start > /dev/null &", $output, $return);
             break;
         case 'server_restart':
-            exec("sudo service tnp-server stop", $output, $return);
-            exec("/usr/bin/sudo /usr/bin/service network-sensor-server start > /dev/null &", $output, $return);
+            exec("sudo service network-sensor-server stop", $output, $return);
+            exec("sudo service network-sensor-server start > /dev/null &", $output, $return);
             break;
         case 'server_stop':
-            exec("/usr/bin/sudo /usr/bin/service network-sensor-server stop", $output, $return);
+            exec("sudo service network-sensor-server stop", $output, $return);
             foreach ($output as $value) {
                 echo("OUTPUT".$value);
             }
